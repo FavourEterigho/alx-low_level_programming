5373153bbac9225025b99eed536372757c46db3a
FavourEterigho <oberaborfavour30@gmail.com>
2023-05-03 01:56:33 -0700

    main.h

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_sting(char *str);
+void puts2(char *str);
+void puts_half(char *str)
+void print_array(int *a, int n);
+char *_strcpv(char *dest, char *src);
+
+#endif
