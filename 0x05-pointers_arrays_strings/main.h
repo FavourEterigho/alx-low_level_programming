853960155a6e0168884ec6f924429c1838efb546
FavourEterigho <oberaborfavour30@gmail.com>
2023-05-26 15:27:03 -0700

    0-object_like_macro.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -14,6 +14,6 @@ void rev_sting(char *str);
 void puts2(char *str);
 void puts_half(char *str)
 void print_array(int *a, int n);
-char *_strcpv(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 
 #endif
